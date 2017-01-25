f8f1ee72d08ec13f479820f339bb00ce88e022d5
gioacostax <gioacostax@gmail.com>
2017-01-24 22:07:46 -0500

    [Demo] Initial application mode

--- demo.js
+++ demo.js
@@ -1,36 +1,25 @@
-// We need this to build our post string
-var http = require('http');
+var fetch = require('node-fetch')
 
-  var post_data = JSON.stringify({ method: 'buscador.obtenerAsignaturas', params: [
-        '',
-        'PRE',
-        'p',
-        'PRE',
-        '2509',
-        '',
-        1,
-        3
-      ] });
-  // An object of options to indicate where to post to
-  var post_options = {
-      host: 'sia.bogota.unal.edu.co',
-      port: '80',
-      path: '/buscador/JSON-RPC',
-      method: 'POST',
-      headers: {
-          'Content-Type': 'text/plain;charset=UTF-8',
-          'Content-Length': Buffer.byteLength(post_data)
-      }
-  };
+var query = JSON.stringify({
+  host: 'sia.bogota.unal.edu.co',
+  query: { method: 'buscador.obtenerAsignaturas', params: [
+    '',
+    'PRE',
+    'p',
+    'PRE',
+    '2509',
+    '',
+    1,
+    3
+  ]}
+});
 
-  // Set up the request
-  var post_req = http.request(post_options, function(res) {
-      res.setEncoding('utf8');
-      res.on('data', function (chunk) {
-          console.log('Response: ' + chunk);
-      });
-  });
-
-  // post the data
-  post_req.write(post_data);
-  post_req.end();
+fetch(`http://localhost/buscador/JSON-RPC`, { 
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: query
+}).then(res => {
+  res.json().then(json => console.log(json)).catch(err => console.log(err));
+}).catch(err => console.log(err));
